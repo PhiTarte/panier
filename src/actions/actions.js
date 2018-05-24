4bc6de4cde962815940da899498d101dd20ad311
philippe tarte <tarteserviceinformatique@sfr.fr>
2018-05-24 09:58:14 +0200

    adding action for adding an article

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,8 +1,14 @@
-export const REMOVE_ARTICLE = "REMOVE_ARTICLE";
+export const REMOVE_ARTICLE = "REMOVE_ARTICLE"
 
 export const makeRemoveArticleAction = articleId => ({
   type: REMOVE_ARTICLE,
   articleId
-});
+})
 
 //TODO: ajouter l'action pour ajouter un article au panier
+
+export const ADD_ARTICLE = "ADD_ARTICLE"
+export const makeNewArticle = article => ({
+  type: ADD_ARTICLE,
+  articleId: 2
+})
--- src/actions/actions.test.js
+++ src/actions/actions.test.js
@@ -1,19 +1,38 @@
-import { REMOVE_ARTICLE, makeRemoveArticleAction } from "./actions";
+import { REMOVE_ARTICLE, makeRemoveArticleAction } from "./actions"
 
 describe("action remove article creator", () => {
   it("should return an REMOVE_ARTICLE action with article id", () => {
     const expected = {
       type: REMOVE_ARTICLE,
       articleId: 2
-    };
+    }
 
-    expect(makeRemoveArticleAction(2)).toEqual(expected);
+    expect(makeRemoveArticleAction(2)).toEqual(expected)
 
     const expectedForAnotherArticle = {
       type: REMOVE_ARTICLE,
       articleId: 5
-    };
+    }
 
-    expect(makeRemoveArticleAction(5)).toEqual(expectedForAnotherArticle);
-  });
-});
+    expect(makeRemoveArticleAction(5)).toEqual(expectedForAnotherArticle)
+  })
+})
+
+describe("action add article creator", () => {
+  it("should return an ADD_ARTICLE action with article id", () => {
+    const ADD_ARTICLE = "ADD_ARTICLE"
+    const expected = {
+      type: ADD_ARTICLE,
+      articleId: 2
+    }
+
+    expect(makeRemoveArticleAction(2)).toEqual(expected)
+
+    const expectedForAnotherArticle = {
+      type: ADD_ARTICLE,
+      articleId: 5
+    }
+
+    expect(makeRemoveArticleAction(5)).toEqual(expectedForAnotherArticle)
+  })
+})
